ce163d192c1f6b71205a0b7ca22f5ec4de8bdad4
DerFetzer <kontakt@der-fetzer.de>
2022-07-01 00:31:38 +0200

    Add unit tests

--- src/converter.rs
+++ src/converter.rs
@@ -49,4 +49,49 @@ impl TraceReader {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn trace_reader_read() {
+        let csv = "timeStamp;ParkFunc;AngleParkFunc\n\
+        3.83;65533.0;4093.0\n\
+        4.76;400.0; "
+            .to_string();
+        let c = Cursor::new(csv);
+
+        let exp = Ok(vec![
+            Sample {
+                timestamp: 3.83,
+                signal_values: HashMap::from([
+                    ("ParkFunc".to_string(), 65533.0),
+                    ("AngleParkFunc".to_string(), 4093.0),
+                ]),
+            },
+            Sample {
+                timestamp: 4.76,
+                signal_values: HashMap::from([("ParkFunc".to_string(), 400.0)]),
+            },
+        ]);
+
+        assert_eq!(TraceReader::read(c), exp);
+    }
+
+    #[test]
+    fn sample_to_json() {
+        let s = Sample {
+            timestamp: 1.112,
+            signal_values: HashMap::from([
+                ("signal1".to_string(), 42.42),
+                ("signal2".to_string(), 3.14),
+            ]),
+        };
+
+        let res = s.to_json();
+
+        // Since HashMap is not ordered singals are in arbitrary order
+        assert!(
+            res == "{\"timestamp\": 1.112, \"signal_values\": {\"signal2\": 3.14, \"signal1\": 42.42}}"
+                || res == "{\"timestamp\": 1.112, \"signal_values\": {\"signal1\": 42.42, \"signal2\": 3.14}}"
+        )
+    }
 }
